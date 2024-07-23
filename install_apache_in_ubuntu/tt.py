b601a1488ac35d35cff1cd34489660dc4ef37aae
parmeshwor <thapa.parmeshwor@gmail.com>
2024-07-23 17:51:50 -0500

    Update tt.py

--- install_apache_in_ubuntu/tt.py
+++ install_apache_in_ubuntu/tt.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import urllib3
-import certifi
-import ssl
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 # URL of the Selenium Grid hub
 grid_url = "https://localhost:4444/wd/hub"
@@ -10,38 +8,26 @@ grid_url = "https://localhost:4444/wd/hub"
 # Path to the self-signed certificate
 cert_path = "path/to/your/selenium_grid_cert.crt"
 
-# Add the certificate to the certifi trust store
-with open(certifi.where(), 'a') as cert_file:
-    with open(cert_path, 'r') as my_cert:
-        cert_file.write(my_cert.read())
-
-# Create a custom HTTPS connection that uses the updated certifi trust store
-class VerifiedHTTPSConnection(urllib3.connection.HTTPSConnection):
-    def __init__(self, *args, **kwargs):
-        kwargs['ssl_context'] = ssl.create_default_context(cafile=certifi.where())
-        super().__init__(*args, **kwargs)
-
-# Create a custom HTTPSConnectionPool that uses VerifiedHTTPSConnection
-class VerifiedHTTPSConnectionPool(urllib3.connectionpool.HTTPSConnectionPool):
-    ConnectionCls = VerifiedHTTPSConnection
-
-# Create a custom PoolManager that uses VerifiedHTTPSConnectionPool
-class VerifiedPoolManager(urllib3.PoolManager):
-    def __init__(self, *args, **kwargs):
-        kwargs['connection_pool_kw'] = kwargs.get('connection_pool_kw', {})
-        kwargs['connection_pool_kw']['ConnectionCls'] = VerifiedHTTPSConnectionPool.ConnectionCls
-        super().__init__(*args, **kwargs)
-
 # Set up Chrome options
 chrome_options = Options()
 chrome_options.add_argument('--ignore-certificate-errors')
 chrome_options.add_argument('--allow-insecure-localhost')
 
+# Use --ignore-certificate-errors-spki-list to ignore the certificate
+with open(cert_path, "r") as cert_file:
+    cert_data = cert_file.read()
+
+# Add the certificate to Chrome's trust store using the --ignore-certificate-errors-spki-list flag
+import base64
+import hashlib
+
+cert_hash = hashlib.sha256(base64.b64decode(cert_data.strip())).digest()
+chrome_options.add_argument(f'--ignore-certificate-errors-spki-list={base64.b64encode(cert_hash).decode()}')
+
 # Create a new instance of the Remote WebDriver with Chrome options
 driver = webdriver.Remote(
     command_executor=grid_url,
-    options=chrome_options,
-    http_client=VerifiedPoolManager()
+    options=chrome_options
 )
 
 # Open a website (example: Google)
